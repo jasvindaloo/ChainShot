30f727cb2d5b99264c19cc346731f7f7eca95c08
jasvindaloo <96028677+jasvindaloo@users.noreply.github.com>
2021-12-16 12:38:51 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,15 +3,28 @@ const app = express();
 const cors = require('cors');
 const port = 3042;
 
+// adding in the thing to generate addresses
+const EC = require('elliptic').ec;
+
+const ec = new EC('secp256k1');
+
+const key = ec.genKeyPair();
+
+console.log({
+  privateKey: key.getPrivate().toString(16),
+  publicX: key.getPublic().x.toString(16),
+  publicY: key.getPublic().y.toString(16),
+});
+
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+  publicX + "" + publicY: 100,
+  publicX + "" + publicY: 50,
+  publicX + "" + publicY: 75,
 }
 
 app.get('/balance/:address', (req, res) => {
